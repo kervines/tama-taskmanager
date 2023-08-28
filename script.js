f6be2851548471104b35294a6301814c4690a74d
kervines <kervinzero@gmail.com>
2023-08-28 18:20:56 -0300

    criada função de criar item

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const form = document.querySelector('.form');
+const taksList = document.querySelector('.task-list');
 
-function toDo() {
-  const activityList = [];
-  let i = 0;
-
-  function addActivity(event) {
-    event.preventDefault();
-  }
-
-  function deleteList() {}
-
-  form.addEventListener('submit', addActivity);
-  btnDeleteAll.addEventListener('click', deleteList);
+function criaItem() {
+  const li = document.createElement('li');
+  li.classList.add('task-item');
+  taksList.appendChild(li);
 }
+criaItem();
 
-toDo();
+function criaParagrafo() {}
+function criaBotoes() {}
+
+function adicionarTarefa() {}
+
+// function toDo() {
+//   const activityList = [];
+//   let i = 0;
+
+//   function addActivity(event) {
+//     event.preventDefault();
+//   }
+
+//   function deleteList() {}
+
+//   form.addEventListener('submit', addActivity);
+//   btnDeleteAll.addEventListener('click', deleteList);
+// }
+
+// toDo();
