4be412984164d649e0ba24e715518149c4b8e869
kervines <kervinzero@gmail.com>
2023-08-28 19:05:02 -0300

    criada funções de criar os botoes

--- script.js
+++ script.js
@@ -6,17 +6,55 @@ function criaItem() {
   const li = document.createElement('li');
   li.classList.add('task-item');
   taksList.appendChild(li);
+
+  const p = criaParagrafo();
+  li.appendChild(p);
+  const containerBtn = criaContainerBotoes();
+  li.appendChild(containerBtn);
 }
 criaItem();
 
 function criaParagrafo() {
   const p = document.createElement('p');
   p.classList.add('action');
-  p.innerText = activity.value;
+  // p.innerText = activity.value;
+  p.innerText = 'EXEMPLO JS';
+  return p;
 }
-criaParagrafo();
 
-function criaBotoes() {}
+function criaContainerBotoes() {
+  const containerBtn = document.createElement('div');
+  containerBtn.classList.add('button-list');
+
+  const btnDone = criaBotaoDone();
+  const btnDel = criaBotaoDel();
+  const btnEdit = criaBotaoEdit();
+  containerBtn.appendChild(btnDone);
+  containerBtn.appendChild(btnDel);
+  containerBtn.appendChild(btnEdit);
+
+  return containerBtn;
+}
+
+function criaBotaoDone() {
+  const btn = document.createElement('button');
+  btn.classList.add('done');
+  btn.innerText = 'Feito';
+  return btn;
+}
+function criaBotaoDel() {
+  const btn = document.createElement('button');
+  btn.classList.add('del');
+  btn.innerText = 'Delete';
+  return btn;
+}
+function criaBotaoEdit() {
+  const btn = document.createElement('button');
+  btn.classList.add('edit');
+  btn.innerText = 'Edite';
+  return btn;
+}
+
 function adicionarTarefa() {}
 
 // function toDo() {
