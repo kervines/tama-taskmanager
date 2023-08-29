44c0fb3d585ea99f21521ac0fb983e326c905b95
kervines <kervinzero@gmail.com>
2023-08-28 21:22:30 -0300

    função de adicionar e feito

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
 const form = document.querySelector('.form');
 const activity = document.querySelector('.activity');
 const taksList = document.querySelector('.task-list');
+const paragrafoAction = document.querySelectorAll('.action');
+
+const btnAdd = document.querySelector('.btn-add');
+const btnDone = document.querySelector('.done');
+// const btnDel = document.querySelector('.del');
+// const btnEdit = document.querySelector('.edit');
 
 function criaItem() {
-  const li = document.createElement('li');
-  li.classList.add('task-item');
-  taksList.appendChild(li);
+  if (!!activity.value) {
+    const li = document.createElement('li');
+    li.classList.add('task-item');
+    taksList.appendChild(li);
 
-  const p = criaParagrafo();
-  li.appendChild(p);
-  const containerBtn = criaContainerBotoes();
-  li.appendChild(containerBtn);
+    const p = criaParagrafo();
+    li.appendChild(p);
+    const containerBtn = criaContainerBotoes(p);
+    li.appendChild(containerBtn);
+
+    return li;
+  } else {
+    return;
+  }
 }
-criaItem();
 
 function criaParagrafo() {
   const p = document.createElement('p');
   p.classList.add('action');
-  // p.innerText = activity.value;
-  p.innerText = 'EXEMPLO JS';
+  p.innerText = activity.value;
   return p;
 }
 
-function criaContainerBotoes() {
+function criaContainerBotoes(paragrafo) {
   const containerBtn = document.createElement('div');
   containerBtn.classList.add('button-list');
 
   const btnDone = criaBotaoDone();
+  btnDone.addEventListener('click', () => done(paragrafo));
+
   const btnDel = criaBotaoDel();
   const btnEdit = criaBotaoEdit();
   containerBtn.appendChild(btnDone);
@@ -36,12 +48,17 @@ function criaContainerBotoes() {
   return containerBtn;
 }
 
+function done(paragrafo) {
+  return (paragrafo.style.textDecoration = 'line-through');
+}
+
 function criaBotaoDone() {
   const btn = document.createElement('button');
   btn.classList.add('done');
   btn.innerText = 'Feito';
   return btn;
 }
+
 function criaBotaoDel() {
   const btn = document.createElement('button');
   btn.classList.add('del');
@@ -55,20 +72,11 @@ function criaBotaoEdit() {
   return btn;
 }
 
-function adicionarTarefa() {}
-
-// function toDo() {
-//   const activityList = [];
-//   let i = 0;
-
-//   function addActivity(event) {
-//     event.preventDefault();
-//   }
-
-//   function deleteList() {}
-
-//   form.addEventListener('submit', addActivity);
-//   btnDeleteAll.addEventListener('click', deleteList);
-// }
-
-// toDo();
+function adicionaTarefa() {
+  function preventForm(event) {
+    event.preventDefault();
+  }
+  btnAdd.addEventListener('click', criaItem);
+  form.addEventListener('submit', preventForm);
+}
+adicionaTarefa();
