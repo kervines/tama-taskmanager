9361bf2c443dcdcf6fec89fb07950944b02caf5a
kervines <kervinzero@gmail.com>
2024-04-02 23:46:05 -0300

    feat - criando funcionalidade js de cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,1 +1,36 @@
-export default function initCadastro() {}
+const inputName = document.querySelector('#name-register');
+const inputEmail = document.querySelector('#email-register');
+const inputPassword = document.querySelector('#password-register');
+const btnRegister = document.querySelector('#btn-register');
+const formRegister = document.querySelector('#form-register');
+
+const slotUsers = JSON.parse(localStorage.getItem('users')) || [];
+const usersLocalStorage = (user) => {
+  slotUsers.push(user);
+  localStorage.setItem('users', JSON.stringify(slotUsers));
+};
+
+formRegister.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const user = {
+    name: inputName.value,
+    email: inputEmail.value,
+    password: inputPassword.value,
+  };
+  usersLocalStorage(user);
+
+  // slotUsers.forEach((user, index, array) => {
+  //   if (index === 0) {
+  //     console.log('primeiro cadastro');
+  //   }
+  //   if (array[index].name === array[index - 1].name) {
+  //     console.log('nome cadastrado');
+  //     return;
+  //   }
+  // });
+
+  inputName.value = '';
+  inputEmail.value = '';
+  inputPassword.value = '';
+});
--- js/login.js
+++ js/login.js
@@ -1,1 +1,17 @@
-export default function initLogin() {}
+const inputEmail = document.querySelector('#email-login');
+const inputPassword = document.querySelector('#password-login');
+const loginBtn = document.querySelector('#login-btn');
+const formLogin = document.querySelector('#form-login');
+
+console.log(inputEmail, inputPassword, loginBtn);
+
+formLogin.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const user = {
+    email: inputEmail.value,
+    password: inputPassword.value,
+  };
+
+  console.log(user);
+});
