6f6252f4fd522382b68b64b0b517f158f09f27ba
kervines <kervinzero@gmail.com>
2024-04-04 00:20:17 -0300

    criando validacao de email no cadastro - com bug

--- js/cadastro.js
+++ js/cadastro.js
@@ -3,11 +3,38 @@ const inputEmail = document.querySelector('#email-register');
 const inputPassword = document.querySelector('#password-register');
 const btnRegister = document.querySelector('#btn-register');
 const formRegister = document.querySelector('#form-register');
+const emailContainer = document.querySelector('.email');
 
-const slotUsers = JSON.parse(localStorage.getItem('users')) || [];
+const slotsUsers = JSON.parse(localStorage.getItem('users')) || [];
+
+const msgErro = (msg, parentElement) => {
+  const p = document.createElement('p');
+  p.style.color = 'red';
+  p.style.fontSize = '.875rem';
+  p.style.marginTop = '.5rem';
+  p.innerText = msg;
+  parentElement.appendChild(p);
+
+  setTimeout(() => {
+    p.remove();
+  }, 2000);
+};
+
 const usersLocalStorage = (user) => {
-  slotUsers.push(user);
-  localStorage.setItem('users', JSON.stringify(slotUsers));
+  if (slotsUsers.length > 0) {
+    slotsUsers.forEach((slot) => {
+      if (slot.email === user.email) {
+        return msgErro('Email inválido', emailContainer);
+      } else {
+        return slotsUsers.push(user);
+      }
+    });
+  }
+
+  if (slotsUsers.length === 0) {
+    slotsUsers.push(user);
+  }
+  localStorage.setItem('users', JSON.stringify(slotsUsers));
 };
 
 formRegister.addEventListener('submit', (e) => {
@@ -15,20 +42,11 @@ formRegister.addEventListener('submit', (e) => {
 
   const user = {
     name: inputName.value,
-    email: inputEmail.value,
+    email: inputEmail.value.toLowerCase(),
     password: inputPassword.value,
   };
-  usersLocalStorage(user);
 
-  // slotUsers.forEach((user, index, array) => {
-  //   if (index === 0) {
-  //     console.log('primeiro cadastro');
-  //   }
-  //   if (array[index].name === array[index - 1].name) {
-  //     console.log('nome cadastrado');
-  //     return;
-  //   }
-  // });
+  usersLocalStorage(user);
 
   inputName.value = '';
   inputEmail.value = '';
